d595cd47109e821c9d46580b59bf3d4f5596456d
AnasDaoud14 <anas.daoud@esprit.tn>
2023-04-13 15:50:27 +0000

    TODO ANGULAR 2 - ANAS DAOUD - 1ALINFO9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 import {FormsModule} from "@angular/forms";
-import { ArticlesComponentComponent } from './articles-component/articles-component.component';
+import { TemplateDrivenFormComponentComponent } from './template-driven-form-component/template-driven-form-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OffresEmploiComponentComponent,
-    ArticlesComponentComponent
+    TemplateDrivenFormComponentComponent
   ],
     imports: [
         BrowserModule,
